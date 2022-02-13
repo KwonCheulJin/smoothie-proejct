3f07d64be8cd8d361b7d153d52788ca941af941d
charles <chkftm12@gmail.com>
2022-02-13 19:52:01 +0900

    chapter.9 test todoItem complete

--- react-router-todo-list/src/components/pageHeader/index.test.tsx
+++ react-router-todo-list/src/components/pageHeader/index.test.tsx
@@ -1,21 +1,96 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import 'jest-styled-components';
 
-import { Router } from 'react-router-dom';
+import { Router, useNavigate } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 
 import { PageHeader } from './index';
 
 describe('<PageHeader />', () => {
   it('renders componect correctly ', () => {
-    const history = createMemoryHistory();
-    history.push('/');
+    const navigator = createMemoryHistory();
+    navigator.push('/');
 
     const { container } = render(
-      <Router history={history}>
+      <Router location={'/'} navigator={navigator}>
         <PageHeader />
       </Router>,
     );
+
+    const label = screen.getByText('할 일 목록');
+    expect(label).toBeInTheDocument();
+    const goBack = screen.queryByText('돌아가기');
+    expect(goBack).not.toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
   });
+
+  it('renders component correctly with /add URL', () => {
+    const navigator = createMemoryHistory();
+    navigator.push('/');
+
+    render(
+      <Router location={'/add'} navigator={navigator}>
+        <PageHeader />
+      </Router>,
+    );
+
+    const label = screen.getByText('할 일 추가');
+    expect(label).toBeInTheDocument();
+    const goBack = screen.getByText('돌아가기');
+    expect(goBack).toBeInTheDocument();
+    expect(goBack.getAttribute('href')).toBe('/');
+  });
+
+  it('renders component correctly with /detail/:id URL', () => {
+    const navigator = createMemoryHistory();
+    navigator.push('/');
+
+    render(
+      <Router location={'/detail/1'} navigator={navigator}>
+        <PageHeader />
+      </Router>,
+    );
+
+    const label = screen.getByText('할 일 상세');
+    expect(label).toBeInTheDocument();
+    const goBack = screen.getByText('돌아가기');
+    expect(goBack).toBeInTheDocument();
+    expect(goBack.getAttribute('href')).toBe('/');
+  });
+  it('renders component correctly with NotFound', () => {
+    const navigator = createMemoryHistory();
+    navigator.push('/');
+
+    render(
+      <Router location={'/not_found'} navigator={navigator}>
+        <PageHeader />
+      </Router>,
+    );
+
+    const label = screen.getByText('에러');
+    expect(label).toBeInTheDocument();
+    const goBack = screen.getByText('돌아가기');
+    expect(goBack).toBeInTheDocument();
+    expect(goBack.getAttribute('href')).toBe('/');
+  });
+  // it('renders component correctly with goBack link', () => {
+  //   const navigator = createMemoryHistory();
+  //   navigator.push('/');
+
+  //   render(
+  //     <Router location={'/not_found'} navigator={navigator}>
+  //       <PageHeader />
+  //     </Router>,
+  //   );
+
+  //   const goBack = screen.getByText('돌아가기');
+  //   userEvent.click(goBack);
+
+  //   const label = screen.getByText('할 일 목록');
+  //   expect(label).toBeInTheDocument();
+  //   expect(goBack).not.toBeInTheDocument();
+  // });
 });
--- react-router-todo-list/src/components/todoItem/index.test.tsx
+++ react-router-todo-list/src/components/todoItem/index.test.tsx
@@ -1,24 +1,57 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import 'jest-styled-components';
 
 import { ToDoItem } from './index';
+import path from 'path';
 
 describe('<ToDoItem />', () => {
-  // it('renders conponent correctly', () => {
-  //   const { container } = render(<ToDoItem label="default value" />);
-  //   const toDoItem = screen.getByText('default value');
-  //   expect(toDoItem).toBeInTheDocument();
-  //   const deleteButton = screen.getByText('삭제');
-  //   expect(deleteButton).toBeInTheDocument();
-  //   expect(container).toMatchSnapshot();
-  // });
-  // it('clicks the delete button', () => {
-  //   const handleClick = jest.fn();
-  //   render(<ToDoItem label="default valut" onDelete={handleClick} />);
-  //   const deleteButton = screen.getByText('삭제');
-  //   expect(handleClick).toHaveBeenCalledTimes(0);
-  //   fireEvent.click(deleteButton);
-  //   expect(handleClick).toHaveBeenCalledTimes(1);
-  // });
+  it('renders conponent correctly', () => {
+    const { container } = render(
+      <Router>
+        <ToDoItem id={1} label="default value" />
+      </Router>,
+    );
+    const toDoItem = screen.getByText('default value');
+    expect(toDoItem).toBeInTheDocument();
+    expect(toDoItem.getAttribute('href')).toBe('/detail/1');
+    const deleteButton = screen.getByText('삭제');
+    expect(deleteButton).toBeInTheDocument();
+    expect(container).toMatchSnapshot();
+  });
+  it('clicks the delete button', () => {
+    const handleClick = jest.fn();
+    render(
+      <Router>
+        <ToDoItem id={1} label="default valut" onDelete={handleClick} />
+      </Router>,
+    );
+    const deleteButton = screen.getByText('삭제');
+    expect(handleClick).toHaveBeenCalledTimes(0);
+    fireEvent.click(deleteButton);
+    expect(handleClick).toHaveBeenCalledTimes(1);
+  });
+  it('clicks the link', () => {
+    const TestComponent = (): JSX.Element => {
+      const { pathname } = useLocation();
+      return (
+        <div>
+          <div>{pathname}</div>
+          <ToDoItem id={1} label="default value" />
+        </div>
+      );
+    };
+    render(
+      <Router>
+        <TestComponent />
+      </Router>,
+    );
+
+    const pathName = screen.getByText('/');
+    expect(pathName).toBeInTheDocument();
+    const todoItem = screen.getByText('default value');
+    fireEvent.click(todoItem);
+    expect(pathName.textContent).toBe('/detail/1');
+  });
 });
